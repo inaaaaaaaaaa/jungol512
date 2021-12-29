3069fb0e1e55ef7a369bfbeed6ea7cf8e391f639
Si7A-00 <Si7A-00@Si7C-01>
2021-12-29 10:45:50 +0900

    1st commit

---
+++ pro1229/src/Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+
+	public static void main(String[] args) {
+		int w = 49;
+		double g = 0.2683;
+		
+//	System.out.print(w + " * " + g + " = " + w * g );
+		System.out.println(w + " * " + g + " = " + w * g);
+//		System.out.printf("%d * %f = %f", w, g, w * g);
+				
+	}
+
+}
